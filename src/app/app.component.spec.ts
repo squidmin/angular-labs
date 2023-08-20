22c81482814d3608d6998ff2efa408ab596e52e0
squidmin <morse.james.r@gmail.com>
2023-08-20 10:57:40 -0500

    Edit: mock TableComponent in AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,22 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {Component} from "@angular/core";
+
+/**
+ * When you're running tests for a component, and the component has dependencies on other child components, you may not
+ * necessarily want to test the child component's behavior as well. Instead, you might want to mock or "stub" out those
+ * child components. For unit tests, you can use "dummy" components to stub out the real ones.
+ */
+@Component({
+  selector: 'app-table',
+  template: ''
+})
+class MockTableComponent {
+}
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent, MockTableComponent]
   }));
 
   it('should create the app', () => {
@@ -23,5 +36,6 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('p')?.textContent).toContain('App Component');
+
   });
 });
