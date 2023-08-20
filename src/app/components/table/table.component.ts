e4f4b773adef1892a7fcd19d4ec6bdab36743c58
squidmin <morse.james.r@gmail.com>
2023-08-20 09:50:29 -0500

    Add: table component placeholder

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+  placeholder: string = JSON.stringify({key: "value"});
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
