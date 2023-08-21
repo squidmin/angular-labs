095773d3232c6a9fce153fc5f61155be993cad0a
squidmin <morse.james.r@gmail.com>
2023-08-20 22:21:54 -0500

    Edit: refactor BigQueryApiService to filter blank objects

--- src/app/scripts/BigQueryApiService.ts
+++ src/app/scripts/BigQueryApiService.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {Row} from "../Row";
 
 const API_URL = 'http://localhost:8080/bigquery/query';
 
@@ -8,10 +9,26 @@ export async function query(bigQueryApiToken: string, requestBody: object[]): Pr
     'bq-api-token': bigQueryApiToken,
   };
   try {
-    // const request = { body: REQUEST_BODY };
-    const request = {body: requestBody};
-    const response = await axios.post(API_URL, request, {headers: HEADERS},);
-    console.log(response.data);
+    let requestItems: object[] = [{}];
+    requestBody.forEach((requestItem: object) => {
+      let row: any = {};
+      Object.keys(requestItem).forEach((key: string) => {
+        if ("" === key) {
+          row[key] = null;
+        }
+      });
+      requestItems.push(row);
+    });
+    requestItems = requestItems.filter((requestItem: Row) => {
+      return requestItem.id === null &&
+        requestItem.creation_timestamp === null &&
+        requestItem.last_update_timestamp === null &&
+        requestItem.column_a === null &&
+        requestItem.column_b === null;
+    });
+    console.log("REQUEST_BODY ==", JSON.stringify({body: requestBody}));
+    const response = await axios.post(API_URL, {body: requestBody}, {headers: HEADERS},);
+    console.log(response);
   } catch (error: any) {
     console.error('Error calling the API', error);
     if (error.response) {
